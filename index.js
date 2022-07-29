2fc0d1fca4bea1d6d919824c208d80dbbcc115ad
LGers <87089817+LGers@users.noreply.github.com>
2022-07-29 09:28:53 +0300

    feat: CRUD, PostController

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import router from "./router.js"
 import Post from './Post.js';
 
 const PORT = 5000;
@@ -7,17 +8,17 @@ const DB_URL = `mongodb+srv://user:user@cluster0.u9gwzwv.mongodb.net/?retryWrite
 
 const app = express();
 app.use(express.json());
-
-app.post('/', async (req, res) => {
-
-  try {
-    const { author, title, content, picture } = req.body;
-    const post = await Post.create({ author, title, content, picture });
-    res.status(200).json(post);
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
+app.use('/api', router);
+// app.post('/', async (req, res) => {
+//
+//   try {
+//     const { author, title, content, picture } = req.body;
+//     const post = await Post.create({ author, title, content, picture });
+//     res.status(200).json(post);
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
 
 async function startApp() {
   try {
